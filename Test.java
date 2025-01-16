2f9ad0b42983082922ab5607c57f70f67ee5d655
Arpit Aggarwal <62698568+arpitaggarwal135@users.noreply.github.com>
2025-01-16 21:21:51 +0530

    removed Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.Stream;
-
-class Test {
-	public static void main(String[] args) {
-		
-		Stream<Integer> st = Stream.ofNullable(null);
-		st.forEach(i -> System.out.println(i));
-	}
-}
